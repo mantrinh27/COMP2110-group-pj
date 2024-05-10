92205a252a1b267dbf111ad4ac2920f9784576de
Thanh Man Trinh <thanhman.trinh@students.mq.edu.au>
2024-05-10 20:55:48 +1000

    Trying another codes for Mood Widget

--- src/components/mood-widget.js
+++ src/components/mood-widget.js
@@ -1,80 +1,56 @@
-import { LitElement, html, css } from 'https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js';
+import {LitElement, html, css} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js';
+import { TaskModel } from '../models.js';
 
 class MoodWidget extends LitElement {
-  static properties = {
-    selectedMood: {type: String}
-  };
-
-  static styles = css`
+  static get styles() {
+    return css`
     :host {
-        display: block;
-        padding: 20px;
-        font-size: 30px;
-        background-color: white;
-        border: 1px solid black;
-    }
-
-    .emojis {
-      display: flex;
-      justify-content: space-around;
-      align-items: center;
-    }
-
-    .emoji {
-      cursor: pointer;
-      border-radius: 30%;
-      padding: 2px;
-    }
-
+      display: block;
+      width: 250px;
+      height: 285px;
+      background-color: white;
+      border: 1px solid black;
+  }
     .title {
-      text-align: center;
-      font-size: 20px;
-      margin-top: -5px;
-      padding: 10px;
       font-weight: bold;
+      margin-bottom: 5px;
     }
-
-    .selected-mood {
-      text-align: center;
-      margin-top: 10px;
-      font-size: 20px;
-    }
-
-    .selected {
-      border: 1px solid orange;
-    }
-  `;
+    `;
+  }
 
   constructor() {
     super();
-    this.selectedMood = ''; // Default mood
+    this.icon = '😊'; // Default to happy icon
+    this.updateIcon(); // Update the icon based on tasks' status
   }
 
-  selectedMood(mood) {
-    if (this.selectedMood === mood) {
-      this.selectedMood = ''; // Deselect emoji when user click the emoji again
+  updateIcon() {
+    const today = new Date();
+    const tasks = TaskModel.getTasksForDay(today);
+    if (tasks.length === 0) {
+      this.icon = '😊'; // No tasks for today, show happy icon
     } else {
-      this.selectedMood = mood;
+      const closestTask = tasks.reduce((prev, current) => {
+        return Math.abs(new Date(prev.due) - today) < Math.abs(new Date(current.due) - today) ? prev : current;
+      });
+      const dueDate = new Date(closestTask.due);
+      if (dueDate < today) {
+        this.icon = '😭'; // Task is overdue, show upset icon
+      } else {
+        this.icon = '😢'; // Task is due today, show worried icon
+      }
     }
+    this.requestUpdate();
   }
 
   render() {
-    let moodText = '';
-    if (this.selectedMood) {
-      moodText = `Today you are feeling ${this.selectedMood}`;
-    }
     return html`
-    <div class="frame">
-    <div class="title">Mood</div>
-    <div class="emojis">
-      <span class="emoji ${this.selectedMood === 'happy' ? 'selected' : ''}" id="happy" @click="${() => this.selectMood('happy')}">😊</span>
-      <span class="emoji ${this.selectedMood === 'calm' ? 'selected' : ''}" id="calm" @click="${() => this.selectMood('calm')}">😌</span>
-      <span class="emoji ${this.selectedMood === 'sad' ? 'selected' : ''}" id="sad" @click="${() => this.selectMood('sad')}">😞</span>
-      <span class="emoji ${this.selectedMood === 'upset' ? 'selected' : ''}" id="upset" @click="${() => this.selectMood('upset')}">😡</span>
-    </div>
-    <div class="selected-mood">${moodText}</div>
-    </div>`;
+        <div class="title">Mood Widget</div>
+        ${this.icon === '😊' ? html`<span class="${this.icon === '😊' ? 'happy' : 'upset'}">${this.icon}</span> Yay, there is no task due for today!` : html`<span class="${this.icon === '😊' ? 'happy' : 'upset'}">${this.icon}</span>`}
+      </div>
+    `;
   }
 }
 
 customElements.define('mood-widget', MoodWidget);
+
